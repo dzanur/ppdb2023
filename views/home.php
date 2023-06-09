8cd5e10771fed63219779ad725a9cfae1a85e9d5
Muchammad Dzanur Ragil Putra <d24nur@gmail.com>
2023-06-09 15:00:18 +0700

    new

--- views/home.php
+++ views/home.php
@@ -1,8 +1,112 @@
-<div class="py-5 bg-primary bg-opacity-25">
-    <div class="container-fluid">
-        <div class="container d-flex">
-            <h1 class="text-dark text-opacity-75">PPDB SMAN 9 Tangerang</h1>
-            <img src="assets/img/hero.png" alt="Hero">
+<div class="p-5 mb-4 bg-body-tertiary rounded-3">
+    <div class="container-fluid py-5">
+        <h1 class="display-5 fw-bold">PPDB 2023</h1>
+        <h1 class="display-9 fw-bold">Website PPDB SMAN 9 Tangerang</h1>
+        <p class="col-md-8 fs-4">Lihat informasi Pendaftaran Peserta Didik Baru</p>
+        <button class="btn btn-success btn-lg" type="button">Lihat Jadwal <i class="fa-solid fa-angles-right"></i></button>
+    </div>
+</div>
+<div class="card">
+  <div class="card-body bg-warning">
+  <i class="fa-solid fa-circle-info"></i> Informasi PPDB 2023
+  </div>
+</div>
+<!-- <div class="container-fluid bg-warning">
+    <div class="container p-3">
+    <i class="fa-solid fa-circle-info"></i> Informasi PPDB 2023
+    </div>
+</div> -->
+<div class="container p-5">
+    <h1 class="text-center mb-3">Jalur Pendaftaran</h1>
+    <div class="row row-cols-1 row-cols-md-3 mb-3 text-center">
+      <div class="col-md-6 col-lg-3">
+        <div class="card mb-4 rounded-3 shadow-sm">
+          <div class="card-header py-3">
+            <h4 class="my-0 fw-normal">Afirmasi</h4>
+          </div>
+          <div class="card-body">
+            <table class="table table-striped">
+                <thead>
+                    <tr>
+                    <th scope="col">#</th>
+                    <th scope="col">First</th>
+                    <th scope="col">Last</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    <tr>
+                    <th scope="row">1</th>
+                    <td>Mark</td>
+                    <td>Otto</td>
+                    <td>@mdo</td>
+                    </tr>
+                    <tr>
+                    <th scope="row">2</th>
+                    <td>Jacob</td>
+                    <td>Thornton</td>
+                    <td>@fat</td>
+                    </tr>
+                    <tr>
+                    <th scope="row">3</th>
+                    <td colspan="2">Larry the Bird</td>
+                    <td>@twitter</td>
+                    </tr>
+                </tbody>
+            </table>
+            <button type="button" class="w-100 btn btn-lg btn-outline-primary">Daftar</button>
+          </div>
         </div>
+      </div>
+      <div class="col-md-6 col-lg-3">
+        <div class="card mb-4 rounded-3 shadow-sm">
+          <div class="card-header py-3">
+            <h4 class="my-0 fw-normal">Zonasi</h4>
+          </div>
+          <div class="card-body">
+            <h1 class="card-title pricing-card-title">$0<small class="text-body-secondary fw-light">/mo</small></h1>
+            <ul class="list-unstyled mt-3 mb-4">
+              <li>10 users included</li>
+              <li>2 GB of storage</li>
+              <li>Email support</li>
+              <li>Help center access</li>
+            </ul>
+            <button type="button" class="w-100 btn btn-lg btn-outline-primary">Daftar</button>
+          </div>
+        </div>
+      </div>
+      <div class="col-md-6 col-lg-3">
+        <div class="card mb-4 rounded-3 shadow-sm">
+          <div class="card-header py-3">
+            <h4 class="my-0 fw-normal">Mutasi</h4>
+          </div>
+          <div class="card-body">
+            <h1 class="card-title pricing-card-title">$0<small class="text-body-secondary fw-light">/mo</small></h1>
+            <ul class="list-unstyled mt-3 mb-4">
+              <li>10 users included</li>
+              <li>2 GB of storage</li>
+              <li>Email support</li>
+              <li>Help center access</li>
+            </ul>
+            <button type="button" class="w-100 btn btn-lg btn-outline-primary">Daftar</button>
+          </div>
+        </div>
+      </div>
+      <div class="col-md-6 col-lg-3">
+        <div class="card mb-4 rounded-3 shadow-sm border-primary">
+          <div class="card-header py-3 text-bg-primary border-primary">
+            <h4 class="my-0 fw-normal">Prestasi</h4>
+          </div>
+          <div class="card-body">
+            <h1 class="card-title pricing-card-title">$29<small class="text-body-secondary fw-light">/mo</small></h1>
+            <ul class="list-unstyled mt-3 mb-4">
+              <li>30 users included</li>
+              <li>15 GB of storage</li>
+              <li>Phone and email support</li>
+              <li>Help center access</li>
+            </ul>
+            <button type="button" class="w-100 btn btn-lg btn-primary">Daftar</button>
+          </div>
+        </div>
+      </div>
     </div>
 </div>

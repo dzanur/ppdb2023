1bd6e73b7bd1f84159f42ee4569dbf9dfe6cc518
Muchammad Dzanur Ragil Putra <d24nur@gmail.com>
2023-06-16 23:44:45 +0700

    new

--- views/admin.php
+++ views/admin.php
@@ -4,7 +4,7 @@
     </h4>
     <div class="row p-5">
         <div class="col-12 col-md-3 py-2 py-md-0">
-            <a href="<?= $$vafirmasi ?>" target="_BLANK">
+            <a href="<?= $$vafirmasi ?>" class="text-decoration-none" target="_BLANK">
                 <div class="card">
                     <div class="card-header bg-danger text-light">
                         Afirmasi
@@ -17,7 +17,7 @@
             </a>
         </div>
         <div class="col-12 col-md-3 py-2 py-md-0">
-            <a href="<?= $dtks ?>" target="_BLANK">
+            <a href="<?= $dtks ?>" class="text-decoration-none" target="_BLANK">
                 <div class="card">
                     <div class="card-header bg-danger-400 text-light">
                         Afirmasi
@@ -30,7 +30,7 @@
             </a>
         </div>
         <div class="col-12 col-md-3 py-2 py-md-0">
-            <a href="<?= $pip ?>" target="_BLANK">
+            <a href="<?= $pip ?>" class="text-decoration-none" target="_BLANK">
                 <div class="card">
                     <div class="card-header bg-danger-400 text-light">
                         Afirmasi

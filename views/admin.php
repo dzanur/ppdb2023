6e74ef568743ca6c6ff0497faf791ea1c07d3cc5
Muchammad Dzanur Ragil Putra <d24nur@gmail.com>
2023-06-22 20:11:04 +0700

    menu

--- views/admin.php
+++ views/admin.php
@@ -1,18 +1,40 @@
-<div class="container">
-    <h4 class="text-center pt-5">
-        Aplikasi Verifikasi
-    </h4>
-    <div class="row pt-3">
-        <div class="col-12">
-            <a href="<?= $vzonasi ?>" class="text-decoration-none" target="_BLANK">
-                <div class="card">
-                    <div class="card-body py-5 bg-primary text-center text-light rounded">
-                        <h5 class="card-title">Verifikasi</h5>
-                        <p class="card-text">Panel Verifikasi Operator</p>
+<h4 class="text-center pt-5">
+    Aplikasi Verifikasi
+</h4>
+<section class="bg-primary-100">
+    <div class="container p-5">
+        <div class="row pt-3">
+            <div class="col-12 col-md-6">
+                <a href="/kemdikbud" class="text-decoration-none" target="_BLANK">
+                    <div class="card">
+                        <div class="card-body py-5 bg-success text-center text-light rounded">
+                            <div class="col">
+                                <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/2/2e/Logo_Kemendikbud.svg/2016px-Logo_Kemendikbud.svg.png" class="img-thumbnail rounded float-start" width="60" alt="Kemdikbud">
+                            </div>
+                            <div class="col">
+                                <h5 class="card-title">Cek Data Kemdikbud</h5>
+                                <p class="card-text">Lihat data peserta</p>
+                            </div>
+
+                        </div>
                     </div>
-                </div>
-            </a>
+                </a>
+            </div>
+            <div class="col-12 col-md-6">
+                <a href="<?= $vzonasi ?>" class="text-decoration-none" target="_BLANK">
+                    <div class="card">
+                        <div class="card-body py-5 bg-primary text-center text-light rounded">
+                            <h5 class="card-title">Verifikasi</h5>
+                            <p class="card-text">Panel Verifikasi Operator</p>
+                        </div>
+                    </div>
+                </a>
+            </div>
         </div>
+    </div>
+</section>
+<div class="container">
+    <div class="row pt-3">
     </div>
     <div class="row pt-3 mb-5">
         <div class="col-12 col-md-4 py-2 py-md-0">
